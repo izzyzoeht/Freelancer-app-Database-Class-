41d0dab98c8790b6183a0145685e1e5e1611ee6c
nchen9029 <nchen055643@gmail.com>
2026-05-11 13:40:34 -0400

    messed up env files, recommiting docker comp and db connection to backend within docker

--- backend/config.py
+++ backend/config.py
@@ -1,25 +1,11 @@
 import os
-# read .env file
 from dotenv import load_dotenv
-
 load_dotenv()
 
-
-class Config: 
-    # clas holdes all app settings in one place
-    # organized and easy to import anywhere in app
-
-    SECRET_KEY = os.getenv('SECRET_KEY' , 'dev-secret-key')
-    #secret key for sessions
-   # detailed error while building 
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
     DEBUG = True
-
-    # Database setting-  need more database info after
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-  #which computer MySQL is running on 
-   #MySQl username 
-    DB_USER = os.getenv('DB_USER','root')
-  #MySql password
-    DB_PASSWORD = os.getenv('DB-PASSWORD','Oli35563471!')
-# Mysql  name
+    DB_HOST = os.getenv('DB_HOST', 'database')
+    DB_USER = os.getenv('DB_USER', 'freelancer')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
     DB_NAME = os.getenv('DB_NAME', 'freelancer_db')
